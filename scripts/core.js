6c4b227ef8b5b9480530fd3fe45ee9a2c8e9296e
Berry Kingdom <=>
2021-05-16 18:52:03 +0200

    a

--- scripts/core.js
+++ scripts/core.js
@@ -109,7 +109,7 @@ if((link == "/index.html") || (link == "/")) {
       
     if('geolocation' in navigator) {
         observeGeoPermission();
-        const button = document.querySelector("permissions__allowlocation");
+        const button = document.querySelector("#permissions__allowlocation");
         button.addEventListener("click", async (e) => {
             await geoData();
         });
